0f52db77a969dfb1a3724ed551f534a493ae3763
Brandon Johnson <brandon121j@gmail.com>
2022-05-03 10:51:20 -0400

    updates to code

--- src/util/apiAxios.js
+++ src/util/apiAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ApiAxios = axios.create({
-  baseURL: 'http://localhost:3001/api/',
+  baseURL: process.env.REACT_APP_AXIOS === 'development' ? 'http://localhost:3001/api' : '/api',
   withCredentials: true,
 });
 
