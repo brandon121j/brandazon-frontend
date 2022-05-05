3a359b0e25f8c2af2752b629f425383b804b21b7
Brandon Johnson <brandon121j@gmail.com>
2022-05-05 14:41:44 -0400

    Removed console log

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -45,7 +45,6 @@ function reducer(state, action) {
 
 function AuthContextComponent({ children }) {
     const [state, dispatch] = useReducer(reducer, initialState);
-    console.log(state);
     return (
         <AuthContext.Provider value={{ state, dispatch }}>
             {children}
